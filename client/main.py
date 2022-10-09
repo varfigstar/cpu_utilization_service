5ed6cb64423de22ddeebd79e69f4c545a297ec3a
Pavel <flamerdazle@yandex.ru>
2022-10-10 01:10:56 +0500

    Add client

---
+++ client/main.py
@@ -0,0 +1,30 @@
+import time
+from argparse import ArgumentParser
+
+import psutil
+import requests
+
+
+parser = ArgumentParser()
+parser.add_argument("--interval", default=5)
+parser.add_argument("--URL", default="http://localhost:8000")
+
+args = parser.parse_args()
+interval = int(args.interval)
+BACKEND_URL = args.URL
+
+
+def main():
+    start = time.time()
+    while True:
+        if time.time() - start < interval:
+            continue
+        cpu_utilization = psutil.cpu_percent()
+        r = requests.post(f"{BACKEND_URL}/api/cpu_info/", data={"utilization_percent": cpu_utilization})
+
+
+if __name__ == "__main__":
+    main()
+
+
+
